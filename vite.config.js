851882ed33dcfb56a3607a857306291aeba23acf
Mikkomario <mikko@victoryfoundations.com>
2026-09-15 13:19:49 -0500

    Prepared the project for (test) deployment

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  resolve: {
-  	alias: {
-	  	'@': fileURLToPath(new URL('.', import.meta.url)), 
-	  	'@tale': fileURLToPath(new URL('./classes', import.meta.url))
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('.', import.meta.url)),
+			'@tale': fileURLToPath(new URL('./classes', import.meta.url))
 		}
-	}, 
-  build: {
-    lib: {
-      entry: 'classes/index.js',
-      formats: ['es'],
-      fileName: 'index'
-    },
-    sourcemap: true
-  }
+	},
+	build: {
+		lib: {
+			entry: 'classes/index.js',
+			formats: ['es'],
+			fileName: 'index'
+		},
+		sourcemap: true
+	}
 });
